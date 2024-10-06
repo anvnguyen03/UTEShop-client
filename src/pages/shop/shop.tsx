91c03598ee27455f9171830632af719d0c89e979
anvnguyen03 <an2572003@gmail.com>
2024-10-06 16:43:03 +0700

    feat: gridlayout for product

--- src/pages/shop/shop.tsx
+++ src/pages/shop/shop.tsx
@@ -1,13 +1,13 @@
-import { useState, useEffect, useContext } from 'react';
+
+import React, { useState, useEffect } from 'react';
 import { Button } from 'primereact/button';
-import { DataView } from 'primereact/dataview';
+import { DataView, DataViewLayoutOptions } from 'primereact/dataview';
 import { Rating } from 'primereact/rating';
 import { Tag } from 'primereact/tag';
 import { classNames } from 'primereact/utils';
 import { ProductService } from './productService';
 import { Dropdown } from 'primereact/dropdown';
 import WebLayout from '../../components/Layout/WebLayout';
-import { SearchContext } from './search';
 
 interface Product {
     id: string;
@@ -22,24 +22,20 @@ interface Product {
     rating: number;
 }
 
-export default function PaginationDemo() {
+export default function BasicDemo() {
     const [products, setProducts] = useState<Product[]>([]);
+    const [layout, setLayout] = useState('grid');
     const [sortKey, setSortKey] = useState('');
     const [sortOrder, setSortOrder] = useState(0);
     const [sortField, setSortField] = useState('');
-    const { searchTerm } = useContext(SearchContext);
     const sortOptions = [
         { label: 'Price High to Low', value: '!price' },
         { label: 'Price Low to High', value: 'price' }
     ];
 
     useEffect(() => {
-        ProductService.getProducts().then((data) => setProducts(data));
+        ProductService.getProducts().then((data) => setProducts(data.slice(0, 12)));
     }, []);
-
-    const filteredProducts = products.filter(product =>
-        product.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
 
     const getSeverity = (product: Product) => {
         switch (product.inventoryStatus) {
@@ -71,18 +67,14 @@ export default function PaginationDemo() {
         }
     };
 
-    const header = () => {
-        return <Dropdown options={sortOptions} value={sortKey} optionLabel="label" placeholder="Sort By Price" onChange={onSortChange} className="w-full sm:w-14rem" />;
-    };
-
-    const itemTemplate = (product: Product, index: number) => {
+    const listItem = (product: Product, index: number) => {
         return (
             <div className="col-12" key={product.id}>
                 <div className={classNames('flex flex-column xl:flex-row xl:align-items-start p-4 gap-4', { 'border-top-1 surface-border': index !== 0 })}>
-                    <a href={`/product?id=${product.id}`}><img className="w-9 sm:w-16rem xl:w-10rem shadow-2 block xl:block mx-auto border-round" src={`https://primefaces.org/cdn/primereact/images/product/${product.image}`} alt={product.name} /></a>
+                    <img className="w-9 sm:w-16rem xl:w-10rem shadow-2 block xl:block mx-auto border-round" src={`https://primefaces.org/cdn/primereact/images/product/${product.image}`} alt={product.name} />
                     <div className="flex flex-column sm:flex-row justify-content-between align-items-center xl:align-items-start flex-1 gap-4">
                         <div className="flex flex-column align-items-center sm:align-items-start gap-3">
-                            <a href={`/product?id=${product.id}`}><div className="text-2xl font-bold text-900">{product.name}</div></a>
+                            <div className="text-2xl font-bold text-900">{product.name}</div>
                             <Rating value={product.rating} readOnly cancel={false}></Rating>
                             <div className="flex align-items-center gap-3">
                                 <span className="flex align-items-center gap-2">
@@ -102,33 +94,66 @@ export default function PaginationDemo() {
         );
     };
 
-    const listTemplate = (items: Product[]) => {
-        if (!items || items.length === 0) return null;
+    const gridItem = (product: Product) => {
+        return (
+            <div className="col-12 sm:col-6 lg:col-12 xl:col-4 p-2" key={product.id}>
+                <div className="p-4 border-1 surface-border surface-card border-round">
+                    <div className="flex flex-wrap align-items-center justify-content-between gap-2">
+                        <div className="flex align-items-center gap-2">
+                            <i className="pi pi-tag"></i>
+                            <span className="font-semibold">{product.category}</span>
+                        </div>
+                        <Tag value={product.inventoryStatus} severity={getSeverity(product)}></Tag>
+                    </div>
+                    <div className="flex flex-column align-items-center gap-3 py-5">
+                        <img className="w-9 shadow-2 border-round" src={`https://primefaces.org/cdn/primereact/images/product/${product.image}`} alt={product.name} />
+                        <div className="text-2xl font-bold">{product.name}</div>
+                        <Rating value={product.rating} readOnly cancel={false}></Rating>
+                    </div>
+                    <div className="flex align-items-center justify-content-between">
+                        <span className="text-2xl font-semibold">${product.price}</span>
+                        <Button icon="pi pi-shopping-cart" className="p-button-rounded" disabled={product.inventoryStatus === 'OUTOFSTOCK'}></Button>
+                    </div>
+                </div>
+            </div>
+        );
+    };
 
-        let list = items.map((product, index) => {
-            return itemTemplate(product, index);
-        });
+    const itemTemplate = (product: Product, layout: string, index: number) => {
+        if (!product) {
+            return;
+        }
 
-        return <div className="grid grid-nogutter">{list}</div>;
+        if (layout === 'list') return listItem(product, index);
+        else if (layout === 'grid') return gridItem(product);
+    };
+
+    const listTemplate = (products: Product[], layout: string) => {
+        return <div className="grid grid-nogutter">{products.map((product, index) => itemTemplate(product, layout, index))}</div>;
+    };
+
+    const header = () => {
+        return (
+            <>
+                <div className='flex justify-content-between mt-6 '>
+                    <Dropdown options={sortOptions} value={sortKey} optionLabel="label" placeholder="Sort By Price" onChange={onSortChange} className="w-full sm:w-14rem" />
+                    <div className="flex justify-content-end">
+                        <DataViewLayoutOptions layout={layout} onChange={(e) => setLayout(e.value)} />
+                    </div>
+                </div>
+                
+            </>
+        );
     };
 
     return (
         <WebLayout>
-            <br />
-            <br />
-            <br />
             <div className="card">
-                <DataView
-                    value={filteredProducts} // Hiển thị sản phẩm đã được lọc
-                    listTemplate={listTemplate}
-                    paginator
-                    rows={5}
-                    header={header()}
-                    sortField={sortField}
-                    sortOrder={sortOrder === 1 || sortOrder === -1 ? sortOrder : null}
-                />
+                <DataView value={products} itemTemplate={itemTemplate} layout={layout} header={header()} sortField={sortField}
+                        sortOrder={sortOrder === 1 || sortOrder === -1 ? sortOrder : null} paginator
+                        rows={6} />
             </div>
         </WebLayout>
-    );
-    
+    )
 }
+        
